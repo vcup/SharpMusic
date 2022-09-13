d19bbb15ae1c76189944e896049245e88bceb4e9
miryo <miryo@vcup.moe>
2022-09-13 21:56:10 +0900

    auto update Artist.JoinedGroup when ArtistsGroup.Members has change

--- src/SharpMusic.Core/Descriptor/ArtistsGroup.cs
+++ src/SharpMusic.Core/Descriptor/ArtistsGroup.cs
@@ -1,14 +1,54 @@
-﻿namespace SharpMusic.Core.Descriptor;
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace SharpMusic.Core.Descriptor;
 
 public class ArtistsGroup : Artist
 {
     internal ArtistsGroup()
     {
         Organizer = new Artist();
-        Members = new List<Artist>();
+        var members = new ImplMembers(this);
+        members.CollectionChanged += (sender, args) =>
+        {
+            if (args.Action.HasFlag(NotifyCollectionChangedAction.Add)
+                && args.NewItems![0] is Artist newItem
+                && newItem.JoinedGroups.All(i => i.Guid != this.Guid))
+            {
+                newItem.JoinedGroups.Add(this);
+            }
+            else if (args.Action.HasFlag(NotifyCollectionChangedAction.Remove) && args.OldItems![0] is Artist removedItem)
+            {
+                removedItem.JoinedGroups.Remove(this);
+            }
+            //else if (args.Action.HasFlag(NotifyCollectionChangedAction.Reset))
+            //{
+            // will do something on override method
+            //}
+        };
+        Members = members;
     }
 
     public Artist Organizer { get; set; }
 
-    public List<Artist> Members { get; }
+    public IList<Artist> Members { get; }
+
+    private class ImplMembers : ObservableCollection<Artist>
+    {
+        private readonly ArtistsGroup _owner;
+
+        public ImplMembers(ArtistsGroup owner)
+        {
+            _owner = owner;
+        }
+
+        protected override void ClearItems()
+        {
+            foreach (var item in this)
+            {
+                item.JoinedGroups.Remove(_owner);
+            }
+            base.ClearItems();
+        }
+    }
 }
