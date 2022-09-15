ccbfe83b7fc51a752b1630147a5d8bd8c24edb31
miryo <miryo@vcup.moe>
2022-09-15 22:31:56 +0900

    console project for test play

---
+++ src/SharpMusic.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using SharpMusic.Core.Player;
+
+var soundSource = new Uri("https://vcup.moe/e/_bazaar records - e^(x+i)＜ 3u.wav");
+var player = new Player();
+player.Open(soundSource);
+
+player.Play();
+Console.ReadKey();
+player.Pause();
+Console.ReadKey();
+player.Resume();
+Console.ReadKey();
+player.Stop();
