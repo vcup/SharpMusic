e1d8a54727bf895c0f1efbe5ebea647494e41caa
miryo <miryo@vcup.moe>
2022-09-18 00:00:12 +0900

    make Music.AlbumsIncluded use CustomObservableImpl

--- src/SharpMusic.Core/Descriptor/Music.cs
+++ src/SharpMusic.Core/Descriptor/Music.cs
@@ -1,4 +1,6 @@
-﻿namespace SharpMusic.Core.Descriptor;
+﻿using System.Collections.Specialized;
+
+namespace SharpMusic.Core.Descriptor;
 
 public class Music : IDescriptor
 {
@@ -7,7 +9,9 @@ public class Music : IDescriptor
         Guid = guid;
         Names = new List<string>();
         Artists = new List<Artist>();
-        AlbumsIncluded = new List<Album>();
+        var albumsIncluded = new CustomObservableImpl<Album>(AlbumsIncludedReset);
+        albumsIncluded.CollectionChanged += AlbumsIncludedAddOrRemoved;
+        AlbumsIncluded = albumsIncluded;
     }
 
     public Music() : this(Guid.NewGuid())
@@ -33,4 +37,29 @@ public class Music : IDescriptor
     /// return the albums of included this music, sort by release date
     /// </summary>
     public IList<Album> AlbumsIncluded { get; }
+
+    private void AlbumsIncludedAddOrRemoved(object? sender, NotifyCollectionChangedEventArgs args)
+    {
+        if (args.Action.HasFlag(NotifyCollectionChangedAction.Add)
+            && args.NewItems![0] is Album newItem
+            && newItem.Tracks.All(i => i.Guid != Guid)
+            && newItem.Tracks is CustomObservableImpl<Music> implA)
+        {
+            implA.AddWithoutNotify(this);
+        }
+        else if (args.Action.HasFlag(NotifyCollectionChangedAction.Remove)
+                 && args.OldItems![0] is Album { Tracks: CustomObservableImpl<Music> implR })
+        {
+            implR.RemoveWithoutNotify(this);
+        }
+    }
+
+    private void AlbumsIncludedReset(object? sender, NotifyCollectionChangedEventArgs args)
+    {
+        if (sender is not CustomObservableImpl<Album> impl) return;
+        foreach (var item in impl)
+        {
+            (item.Tracks as CustomObservableImpl<Music>)!.RemoveWithoutNotify(this);
+        }
+    }
 }
