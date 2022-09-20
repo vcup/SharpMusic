3011c66e2a89ce1ac4c3ed786acdf05fbf76bd91
miryo <miryo@vcup.moe>
2022-09-21 01:18:10 +0900

    add DescriptorProviderBase

---
+++ src/SharpMusic.Core/Management/DescriptorProviderBase.cs
@@ -0,0 +1,34 @@
+﻿namespace SharpMusic.Core.Management;
+
+public abstract class DescriptorProviderBase : IDisposable
+{
+    protected bool IsDisposed;
+    protected readonly DescriptorManager Manager;
+
+    public DescriptorProviderBase(DescriptorManager manager)
+    {
+        Manager = manager;
+    }
+
+    public abstract void Execute();
+
+    public virtual async Task ExecuteAsync()
+    {
+        await Task.Run(Execute);
+    }
+
+    protected void Dispose(bool disposing)
+    {
+        if (!IsDisposed)
+        {
+            // do something
+        }
+
+        IsDisposed = true;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+    }
+}
