7a7ac135184236b77cea8887615f71d556df3b4a
miryo <miryo@vcup.moe>
2022-10-03 01:26:12 +0900

    feat: add minimize/close button

--- src/SharpMusic.GUI/ViewModels/MainWindowViewModel.cs
+++ src/SharpMusic.GUI/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,11 @@
-﻿namespace SharpMusic.GUI.ViewModels;
+﻿using System.Reactive;
+using ReactiveUI;
+
+namespace SharpMusic.GUI.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    public ReactiveCommand<Unit, Unit> MinimizeWindowCommand { get; } = ReactiveCommand.Create(() => { });
+
+    public ReactiveCommand<Unit, Unit> CloseWindowCommand { get; } = ReactiveCommand.Create(() => { });
 }
--- src/SharpMusic.GUI/Views/MainWindow.axaml.cs
+++ src/SharpMusic.GUI/Views/MainWindow.axaml.cs
@@ -1,11 +1,17 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.ReactiveUI;
+using ReactiveUI;
+using SharpMusic.GUI.ViewModels;
 
 namespace SharpMusic.GUI.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 {
     public MainWindow()
     {
         InitializeComponent();
+        this.WhenActivated(b => b(ViewModel!.MinimizeWindowCommand.Subscribe(_ => WindowState = WindowState.Minimized)));
+        this.WhenActivated(b => b(ViewModel!.CloseWindowCommand.Subscribe(_ => Close())));
     }
 }
