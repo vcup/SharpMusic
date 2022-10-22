79f05316d7631fd9a16869090318be29365c7cba
miryo <miryo@vcup.moe>
2022-10-22 17:27:45 +0900

    add FFmpegResampler.cs

---
+++ src/SharpMusic.DllHellP/LowLevelImpl/FFmpegResampler.cs
@@ -0,0 +1,122 @@
+﻿using System.Runtime.InteropServices;
+using FFmpeg.AutoGen;
+using static FFmpeg.AutoGen.ffmpeg;
+
+namespace SharpMusic.DllHellP.LowLevelImpl;
+
+public class FFmpegResampler : IDisposable
+{
+    private readonly unsafe AVCodecContext* _codecCtx;
+    private readonly AVSampleFormat _outFormat;
+    private readonly AVChannelLayout _outChannel;
+    private readonly int _outSampleRate;
+    private readonly unsafe SwrContext* _swrCtx;
+    private bool _isDisposed;
+
+    public unsafe FFmpegResampler(AVCodecContext* codecCtx, AVSampleFormat outFormat, AVChannelLayout outChannel,
+        int outSampleRate)
+    {
+        _codecCtx = codecCtx;
+        _outFormat = outFormat;
+        _outChannel = outChannel;
+        _outSampleRate = outSampleRate;
+        fixed (SwrContext** swrCtx = &_swrCtx)
+        fixed (AVChannelLayout* pOutChannel = &_outChannel)
+        {
+            var ret = swr_alloc_set_opts2(swrCtx, pOutChannel, _outFormat, _outSampleRate,
+                &codecCtx->ch_layout, codecCtx->sample_fmt, codecCtx->sample_rate, 0, null);
+            if (ret < 0) throw new ArgumentException();
+        }
+    }
+
+    public unsafe byte[] ResampleFrame(AVFrame* frame)
+    {
+        long nbSamples;
+        var maxNbSamples = nbSamples = av_rescale_rnd(
+            frame->nb_samples, _outSampleRate, _codecCtx->sample_rate, AVRounding.AV_ROUND_UP
+        );
+
+        byte** resampledData = null;
+        var lineSize = 0;
+        var ret = av_samples_alloc_array_and_samples(
+            &resampledData, &lineSize, _outChannel.nb_channels, (int)nbSamples, _outFormat, 0
+        );
+        if (ret < 0)
+        {
+            throw new ArgumentException();
+        }
+
+        nbSamples = av_rescale_rnd(
+            swr_get_delay(_swrCtx, _codecCtx->sample_rate) +
+            frame->nb_samples, _outSampleRate, _codecCtx->sample_rate, AVRounding.AV_ROUND_UP
+        );
+        if (nbSamples <= 0)
+        {
+            throw new ArgumentException();
+        }
+
+        if (nbSamples > maxNbSamples && *resampledData is not null)
+        {
+            av_free(*resampledData);
+            ret = av_samples_alloc(
+                resampledData, &lineSize, _outChannel.nb_channels, (int)nbSamples, _outFormat, 1
+            );
+
+            if (ret < 0)
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        int resampledDataSize;
+        if (_swrCtx is not null)
+        {
+            ret = swr_convert(_swrCtx, resampledData, (int)nbSamples, frame->extended_data, frame->nb_samples);
+            if (ret < 0)
+            {
+                throw new ArgumentException();
+            }
+
+            resampledDataSize = av_samples_get_buffer_size(&lineSize, _outChannel.nb_channels, ret, _outFormat, 1);
+
+            if (resampledDataSize < 0)
+            {
+                throw new ArgumentException();
+            }
+        }
+        else
+        {
+            throw new ArgumentException();
+        }
+
+        var result = new byte[resampledDataSize];
+
+        Marshal.Copy((IntPtr)(*resampledData), result, 0, resampledDataSize);
+
+        if (*resampledData is not null)
+        {
+            av_freep(resampledData);
+        }
+
+        av_freep(&resampledData);
+
+        return result;
+    }
+
+    public void Dispose()
+    {
+        Dispose(!_isDisposed);
+        GC.SuppressFinalize(this);
+    }
+
+    private unsafe void Dispose(bool disposing)
+    {
+        if (!disposing && _isDisposed) return;
+        fixed (SwrContext** swrCtx = &_swrCtx)
+        {
+            swr_free(swrCtx);
+        }
+
+        _isDisposed = true;
+    }
+}
