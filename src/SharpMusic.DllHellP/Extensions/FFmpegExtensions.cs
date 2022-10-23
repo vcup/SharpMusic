e4e172272362f79d889f1e06be3c9eaf21d7ed3d
miryo <miryo@vcup.moe>
2022-10-23 22:38:01 +0900

    fix: if raw sample rate is 0 will throw

--- src/SharpMusic.DllHellP/Extensions/FFmpegExtensions.cs
+++ src/SharpMusic.DllHellP/Extensions/FFmpegExtensions.cs
@@ -19,12 +19,14 @@ public static class FFmpegExtensions
 
     public static SampleFormat ToSampleFormat(this AVCodecParameters parameters)
     {
-        return parameters.bits_per_raw_sample switch
-        {
-            8 when parameters.format is 0 => SampleFormat.Unsigned8,
-            16 when parameters.format > 0 => SampleFormat.Signed16,
-            32 when parameters.format > 0 => SampleFormat.Signed32,
-            _ => throw new ArgumentOutOfRangeException(nameof(parameters), parameters, null)
-        };
+        return (parameters.bits_per_raw_sample is not 0
+                ? parameters.bits_per_raw_sample
+                : parameters.bits_per_coded_sample) switch
+            {
+                8 when parameters.format is 0 => SampleFormat.Unsigned8,
+                16 when parameters.format > 0 => SampleFormat.Signed16,
+                32 when parameters.format > 0 => SampleFormat.Signed32,
+                _ => throw new ArgumentOutOfRangeException(nameof(parameters), parameters, null)
+            };
     }
 }
