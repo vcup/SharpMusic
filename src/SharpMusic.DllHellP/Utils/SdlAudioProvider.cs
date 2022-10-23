2bb3f1c50650e9cccfb015b85984eb98dde3aa02
miryo <miryo@vcup.moe>
2022-10-23 15:36:37 +0900

    fix: huge memory leak

--- src/SharpMusic.DllHellP/Utils/SdlAudioProvider.cs
+++ src/SharpMusic.DllHellP/Utils/SdlAudioProvider.cs
@@ -7,44 +7,41 @@ namespace SharpMusic.DllHellP.Utils;
 
 public class SdlAudioProvider
 {
-    private readonly IEnumerable<AVFrame> _frames;
+    private readonly IEnumerator<AVFrame> _frames;
     private readonly FFmpegResampler _resampler;
     private byte[] _audioBuffer;
     private int _index;
 
     public SdlAudioProvider(IEnumerable<AVFrame> frames, FFmpegResampler resampler)
     {
-        _frames = frames;
+        _frames = frames.GetEnumerator();
         _resampler = resampler;
         _audioBuffer = Array.Empty<byte>();
     }
 
     public unsafe void AudioCallback(IntPtr userdata, IntPtr stream, int len)
     {
-        while (len > 0)
+        while (len > 0 && _frames.MoveNext())
         {
-            foreach (var frame in _frames)
+            var frame = _frames.Current;
+            if (_index >= _audioBuffer.Length)
             {
-                if (_index >= _audioBuffer.Length)
-                {
-                    _audioBuffer = _resampler.ResampleFrame(&frame);
-                    _index = 0;
-                    Debug.Assert(_audioBuffer.Length is not 0);
-                }
+                _audioBuffer = _resampler.ResampleFrame(&frame);
+                _index = 0;
+                Debug.Assert(_audioBuffer.Length is not 0);
+            }
 
-                var processLen = _audioBuffer.Length - _index;
-                if (processLen > len)
-                {
-                    processLen = len;
-                }
+            var processLen = _audioBuffer.Length - _index;
+            if (processLen > len)
+            {
+                processLen = len;
+            }
 
                 Marshal.Copy(_audioBuffer, _index, stream, processLen);
 
-                len -= processLen;
-                stream += processLen;
-                _index += processLen;
-                if (len is 0) break;
-            }
+            len -= processLen;
+            stream += processLen;
+            _index += processLen;
         }
     }
 }
