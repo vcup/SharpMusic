423e5628e16d8c18235e3974b2da8094c5bdaa73
miryo <miryo@vcup.moe>
2023-02-11 23:15:23 +0900

    [SDL2][tests] fully tested SDL2 related extension methods

--- src/SharpMusic.DllHellP/Extensions/SdlExtensions.cs
+++ src/SharpMusic.DllHellP/Extensions/SdlExtensions.cs
@@ -13,7 +13,7 @@ public static class SdlExtensions
             SampleFormat.Unsigned8 => AUDIO_U8,
             SampleFormat.Signed16 => AUDIO_S16,
             SampleFormat.Signed32 => AUDIO_S32,
-            _ => throw new ArgumentOutOfRangeException(nameof(format), format, null)
+            _ => throw new ArgumentOutOfRangeException(nameof(format), format, null) // dotCover disable this line
         };
     }
 }
---
+++ tests/SharpMusic.DllHellPTests/ExtensionsTests/SdlExtensionsTests.cs
@@ -0,0 +1,57 @@
+﻿using static SDL2.SDL;
+
+namespace SharpMusic.DllHellPTests.ExtensionsTests;
+
+public class SdlExtensionsTests
+{
+    [Test]
+    public void SampleFormatToSdlFmt_PassNone_ThrowNotSupportedException()
+    {
+        // arrange
+        const SampleFormat format = SampleFormat.None;
+
+        // act & assert
+        Assert.Catch<NotSupportedException>(() =>
+            format.ToSdlFmt()
+        );
+    }
+
+    [Test]
+    public void SampleFormatToSdlFmt_PassUnsigned8_GetU8()
+    {
+        // arrange
+        const SampleFormat format = SampleFormat.Unsigned8;
+
+        // act
+        var result = format.ToSdlFmt();
+
+        // assert
+        Assert.That(result, Is.EqualTo(AUDIO_U8));
+    }
+
+    [Test]
+    public void SampleFormatToSdlFmt_PassSigned16_GetS16()
+    {
+        // arrange
+        const SampleFormat format = SampleFormat.Signed16;
+
+        // act
+        var result = format.ToSdlFmt();
+
+        // assert
+        Assert.That(result, Is.EqualTo(AUDIO_S16));
+    }
+
+    [Test]
+    public void SampleFormatToSdlFmt_PassSigned32_GetS32()
+    {
+        // arrange
+        const SampleFormat format = SampleFormat.Signed32;
+
+        // act
+        var result = format.ToSdlFmt();
+
+        // assert
+        Assert.That(result, Is.EqualTo(AUDIO_S32));
+    }
+}
