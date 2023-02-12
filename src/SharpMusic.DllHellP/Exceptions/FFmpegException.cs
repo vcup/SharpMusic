b6fc7e186ca797a6f6a3f65508072c6cdccbdc49
miryo <miryo@vcup.moe>
2023-02-12 14:58:12 +0900

    [FFmpeg][Exception] optimizing FFmpegException implement

--- src/SharpMusic.DllHellP/Exceptions/FFmpegException.cs
+++ src/SharpMusic.DllHellP/Exceptions/FFmpegException.cs
@@ -4,15 +4,34 @@ namespace SharpMusic.DllHellP.Exceptions;
 
 public class FFmpegException : Exception
 {
-    public FFmpegException(string message) : base(message)
+    private readonly string? _errorString;
+    private readonly string? _message;
+
+    public FFmpegException(string message)
     {
+        _message = message;
     }
 
-    public FFmpegException(string message, int errorCode) : base($"{message}->{AvStringErrorCode(errorCode)}")
+    public FFmpegException(string message, int errorCode)
     {
+        ErrorCode = errorCode < 0 ? errorCode : -errorCode;
+        _errorString = AvStringErrorCode(errorCode);
+        _message = message;
     }
 
-    public FFmpegException(int errorCode) : base(AvStringErrorCode(errorCode))
+    public FFmpegException(int errorCode)
     {
+        ErrorCode = errorCode < 0 ? errorCode : -errorCode;
+        _errorString = AvStringErrorCode(errorCode);
     }
+
+    public int ErrorCode { get; init; }
+
+    public string ErrorString => _errorString ?? string.Empty;
+
+    public string MessageString => _message ?? string.Empty;
+
+    public override string Message => _message is null || _errorString is null
+        ? _message ?? _errorString ?? base.Message
+        : _message + " -> " + _errorString;
 }
