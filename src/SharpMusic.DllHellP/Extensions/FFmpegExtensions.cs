572b919be00a388754bf28383b7afdc489e01549
miryo <miryo@vcup.moe>
2023-02-13 01:15:36 +0900

    [FFmpeg][SDL2] update format convert extension methods

--- src/SharpMusic.DllHellP/Extensions/FFmpegExtensions.cs
+++ src/SharpMusic.DllHellP/Extensions/FFmpegExtensions.cs
@@ -12,7 +12,18 @@ public static class FFmpegExtensions
             SampleFormat.None => AVSampleFormat.AV_SAMPLE_FMT_NONE,
             SampleFormat.Unsigned8 => AVSampleFormat.AV_SAMPLE_FMT_U8,
             SampleFormat.Signed16 => AVSampleFormat.AV_SAMPLE_FMT_S16,
+            SampleFormat.Unsigned16 => throw new NotSupportedException(),
             SampleFormat.Signed32 => AVSampleFormat.AV_SAMPLE_FMT_S32,
+            SampleFormat.Float32 => AVSampleFormat.AV_SAMPLE_FMT_FLT,
+            SampleFormat.Double => AVSampleFormat.AV_SAMPLE_FMT_DBL,
+            SampleFormat.Unsigned8Planar => AVSampleFormat.AV_SAMPLE_FMT_U8P,
+            SampleFormat.Signed16Planar => AVSampleFormat.AV_SAMPLE_FMT_S16P,
+            SampleFormat.Signed32Planar => AVSampleFormat.AV_SAMPLE_FMT_S32P,
+            SampleFormat.Float32Planar => AVSampleFormat.AV_SAMPLE_FMT_FLTP,
+            SampleFormat.DoublePlanar => AVSampleFormat.AV_SAMPLE_FMT_DBLP,
+            SampleFormat.Signed64 => AVSampleFormat.AV_SAMPLE_FMT_S64,
+            SampleFormat.Signed64Planar => AVSampleFormat.AV_SAMPLE_FMT_S64P,
+            SampleFormat.Other => AVSampleFormat.AV_SAMPLE_FMT_NB,
             _ => // already cover all case of SampleFormat enum
                 // dotCover disable next line
                 throw new ArgumentOutOfRangeException(nameof(format), format, null)
@@ -27,12 +38,23 @@ public static class FFmpegExtensions
             throw new
                 ArgumentException(message, nameof(parameters));
         }
+
         return parameters->format switch
         {
-            -1 => SampleFormat.None,
-            0 => SampleFormat.Unsigned8,
-            1 => SampleFormat.Signed16,
-            2 => SampleFormat.Signed32,
+            (int)AVSampleFormat.AV_SAMPLE_FMT_NONE => SampleFormat.None,
+            (int)AVSampleFormat.AV_SAMPLE_FMT_U8 => SampleFormat.Unsigned8,
+            (int)AVSampleFormat.AV_SAMPLE_FMT_S16 => SampleFormat.Signed16,
+            (int)AVSampleFormat.AV_SAMPLE_FMT_S32 => SampleFormat.Signed32,
+            (int)AVSampleFormat.AV_SAMPLE_FMT_FLT => SampleFormat.Float32,
+            (int)AVSampleFormat.AV_SAMPLE_FMT_DBL => SampleFormat.Double,
+            (int)AVSampleFormat.AV_SAMPLE_FMT_U8P => SampleFormat.Unsigned8Planar,
+            (int)AVSampleFormat.AV_SAMPLE_FMT_S16P => SampleFormat.Signed16Planar,
+            (int)AVSampleFormat.AV_SAMPLE_FMT_S32P => SampleFormat.Signed32Planar,
+            (int)AVSampleFormat.AV_SAMPLE_FMT_FLTP => SampleFormat.Float32Planar,
+            (int)AVSampleFormat.AV_SAMPLE_FMT_DBLP => SampleFormat.DoublePlanar,
+            (int)AVSampleFormat.AV_SAMPLE_FMT_S64 => SampleFormat.Signed64,
+            (int)AVSampleFormat.AV_SAMPLE_FMT_S64P => SampleFormat.Signed64Planar,
+            (int)AVSampleFormat.AV_SAMPLE_FMT_NB => SampleFormat.Other,
             // TODO: throw message too vague
             _ => throw new ArgumentOutOfRangeException(nameof(parameters), (IntPtr)parameters, null)
         };
--- src/SharpMusic.DllHellP/Extensions/SdlExtensions.cs
+++ src/SharpMusic.DllHellP/Extensions/SdlExtensions.cs
@@ -11,8 +11,19 @@ public static class SdlExtensions
         {
             SampleFormat.None => throw new NotSupportedException(),
             SampleFormat.Unsigned8 => AUDIO_U8,
-            SampleFormat.Signed16 => AUDIO_S16,
-            SampleFormat.Signed32 => AUDIO_S32,
+            SampleFormat.Signed16 => AUDIO_S16SYS,
+            SampleFormat.Unsigned16 => AUDIO_U16SYS,
+            SampleFormat.Signed32 => AUDIO_S32SYS,
+            SampleFormat.Float32 => AUDIO_F32SYS,
+            SampleFormat.Double or
+                SampleFormat.Unsigned8Planar or
+                SampleFormat.Signed16Planar or
+                SampleFormat.Signed32Planar or
+                SampleFormat.Float32Planar or
+                SampleFormat.DoublePlanar or
+                SampleFormat.Signed64 or
+                SampleFormat.Signed64Planar or
+                SampleFormat.Other => AUDIO_S16SYS,
             _ => // already cover all case of SampleFormat enum
                 // dotCover disable next line
                 throw new ArgumentOutOfRangeException(nameof(format), format, null)
