194f326b9cfd019d239177aa16177a0328620077
miryo <miryo@vcup.moe>
2023-03-12 13:57:37 +0900

    [CLI] simply control playback manager with Console Key

--- src/SharpMusic.Console/Program.cs
+++ src/SharpMusic.Console/Program.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
+using System.Text;
 using SharpMusic.Core.Descriptor;
 using SharpMusic.Core.Management;
+using SharpMusic.DllHellP.Utils;
 using static SDL2.SDL;
 
 const string album0 = @"https://vcup.moe/Share/SharpMusic.DllHellPTests/SoundSourceTests`0.flac";
@@ -22,17 +24,49 @@ manager.Playlist.Add(new Music
     SoundSource = { new Uri(album2) }
 });
 
-manager.PlayOrResume();
-Console.ReadLine();
-manager.PlayNext();
-Console.ReadLine();
-manager.PlayNext();
-Console.ReadLine();
-manager.PlayNext();
+manager.Volume = 35;
 
-Console.ReadLine();
-manager.PlayPrev();
-Console.ReadLine();
-manager.PlayPrev();
-
-return 0;
+Console.OutputEncoding = Encoding.UTF8;
+while (true)
+{
+    var info = Console.ReadKey();
+    switch (info.Key)
+    {
+        case ConsoleKey.MediaPlay:
+        case ConsoleKey.P:
+            if (manager.PlaybackState is PlaybackState.Playing) manager.Pause();
+            else manager.PlayOrResume();
+            break;
+        case ConsoleKey.MediaNext:
+        case ConsoleKey.RightArrow when (info.Modifiers & ConsoleModifiers.Control) is not 0:
+        case ConsoleKey.N:
+            manager.PlayNext();
+            break;
+        case ConsoleKey.MediaPrevious:
+        case ConsoleKey.LeftArrow when (info.Modifiers & ConsoleModifiers.Control) is not 0:
+        case ConsoleKey.B:
+            manager.PlayPrev();
+            break;
+        case ConsoleKey.VolumeMute:
+        case ConsoleKey.M:
+            manager.Mute();
+            break;
+        case ConsoleKey.UpArrow:
+            manager.Volume += 5;
+            break;
+        case ConsoleKey.DownArrow:
+            manager.Volume -= 5;
+            break;
+        case ConsoleKey.LeftArrow:
+            manager.PlayPosition -= TimeSpan.FromSeconds(5);
+            break;
+        case ConsoleKey.RightArrow:
+            manager.PlayPosition += TimeSpan.FromSeconds(5);
+            break;
+        case ConsoleKey.Backspace when (info.Modifiers & ConsoleModifiers.Control) is not 0:
+            return;
+    }
+    Console.SetCursorPosition(0, Console.GetCursorPosition().Top);
+    Console.WriteLine($"{info.KeyChar}:{Path.GetFileName(manager.PlayingSoundInfo.Uri.OriginalString)}");
+    Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 1);
+}
