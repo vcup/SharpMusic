c420967ba8f7266599d42633cbf7fbcc1cdf96f9
miryo <miryo@vcup.moe>
2023-04-18 01:16:53 +0900

    [tests] sort codes

--- tests/SharpMusic.DllHellPTests/LowLevelImplTests/FFmpegResamplerTests.cs
+++ tests/SharpMusic.DllHellPTests/LowLevelImplTests/FFmpegResamplerTests.cs
@@ -6,7 +6,8 @@ namespace SharpMusic.DllHellPTests.LowLevelImplTests;
 public class FFmpegResamplerTests
 {
     // ReSharper disable once InconsistentNaming
-    private static AVSampleFormat[] WriteFrame_DataBufferOneByOne_WriteCorrect__Formats = {
+    private static AVSampleFormat[] WriteFrame_DataBufferOneByOne_WriteCorrect__Formats =
+    {
         AVSampleFormat.AV_SAMPLE_FMT_U8,
         AVSampleFormat.AV_SAMPLE_FMT_U8P,
         AVSampleFormat.AV_SAMPLE_FMT_S16,
@@ -16,21 +17,25 @@ public class FFmpegResamplerTests
         AVSampleFormat.AV_SAMPLE_FMT_DBL,
         AVSampleFormat.AV_SAMPLE_FMT_DBLP,
     };
+
     [Test]
     public unsafe void WriteFrame_DataBufferOneByOne_WriteCorrect(
-        [ValueSource(nameof(WriteFrame_DataBufferOneByOne_WriteCorrect__Formats))] AVSampleFormat format,
+        [ValueSource(nameof(WriteFrame_DataBufferOneByOne_WriteCorrect__Formats))]
+        AVSampleFormat format,
         [Values(8000, 44100, 48000)] int length)
     {
         // arrange
         var chLayout = StereoChannelLayout;
+        var data = Generator.RandomSampleData(length, format, chLayout);
+
+        using var ffFrame = new FFFrame(format, chLayout, length);
+        var frame = ffFrame.Frame;
+
         using var ffEncoder = new FFEncoder(AVCodecID.AV_CODEC_ID_FIRST_AUDIO, format, &chLayout);
         var encoder = ffEncoder.CodecCtx;
-        using var ffFrame = new FFFrame(format, chLayout, length);
+
         using var resampler = new FFmpegResampler((IntPtr)encoder,
             format, encoder->ch_layout, encoder->sample_rate, false);
-
-        var frame = ffFrame.Frame;
-        var data = Generator.RandomSampleData(length, format, chLayout);
 
 
         // act
