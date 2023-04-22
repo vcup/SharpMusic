1ddb28ea720fc7f018a37502c8307d900a29bc6d
miryo <miryo@vcup.moe>
2023-04-22 19:16:20 +0900

    [tests] util for cut samples from data[,]

--- tests/SharpMusic.DllHellPTests/Utils/SampleDataHelper.cs
+++ tests/SharpMusic.DllHellPTests/Utils/SampleDataHelper.cs
@@ -19,4 +19,36 @@ public static class SampleDataHelper
 
         return result;
     }
+
+    /// <summary>
+    /// cut data with specify amount of samples
+    /// </summary>
+    /// <param name="data">will cut data</param>
+    /// <param name="format">sample format of data</param>
+    /// <param name="channelLayout">channel layout of data</param>
+    /// <param name="amount">number of samples will cut, negative mean cut end bytes, positive mean cut start bytes, do noting when zero</param>
+    /// <returns>removed samples from data, data itself when amount is 0</returns>
+    public static byte[,] CutSamples(byte[,] data, AVSampleFormat format, AVChannelLayout channelLayout, int amount)
+    {
+        if (amount is 0) return data;
+        var cutEnd = amount < 0;
+        if (cutEnd) amount = -amount;
+
+        var bps = av_get_bytes_per_sample(format);
+        var amountByte = amount * bps;
+        if (av_sample_fmt_is_planar(format) is 0) amountByte *= channelLayout.nb_channels;
+
+        var height = av_sample_fmt_is_planar(format) is 0 ? 1 : data.GetLength(0);
+        var result = new byte[height, data.GetLength(1) - amountByte];
+
+        for (var i = 0; i < height; i++)
+        {
+            for (int j = 0, jd = cutEnd ? 0 : amountByte; j < result.GetLength(1); j++)
+            {
+                result[i, j] = data[i, jd++];
+            }
+        }
+
+        return result;
+    }
 }
