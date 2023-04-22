5b7bce75f682c93d57da73fe672b2799e756c3d9
miryo <miryo@vcup.moe>
2023-04-22 19:23:56 +0900

    [tests][Wrapper] new test for WriteFrame when data more than buffer

--- tests/SharpMusic.DllHellPTests/LowLevelImplTests/FFmpegResamplerTests.cs
+++ tests/SharpMusic.DllHellPTests/LowLevelImplTests/FFmpegResamplerTests.cs
@@ -46,4 +46,47 @@ public class FFmpegResamplerTests
         Assert.That(netRemainingSamples, Is.EqualTo(0));
         CollectionAssert.AreEqual(data, ffFrame);
     }
+
+    // ReSharper disable once InconsistentNaming
+    private static AVSampleFormat[] WriteFrame_DataMoreThanBuffer_WritePossibleAndCache__Formats =
+            WriteFrame_DataBufferOneByOne_WriteCorrect__Formats.ToArray();
+
+    // ReSharper disable once InconsistentNaming
+    private static (int data, int frame, int @out)[] WriteFrame_DataMoreThanBuffer_WritePossibleAndCache__Lengths =
+    {
+        (8000, 7999, -1),
+        (2048, 1024, -1024),
+        (4096, 1024, -3072),
+    };
+
+    [Test]
+    public unsafe void WriteFrame_DataMoreThanBuffer_WritePossibleAndCache(
+        [ValueSource(nameof(WriteFrame_DataMoreThanBuffer_WritePossibleAndCache__Formats))]
+        AVSampleFormat format,
+        [ValueSource(nameof(WriteFrame_DataMoreThanBuffer_WritePossibleAndCache__Lengths))]
+        (int data, int frame, int @out) length
+    )
+    {
+        // arrange
+        var chLayout = StereoChannelLayout;
+        var data = SampleDataHelper.RandomSampleData(length.data, format, chLayout);
+
+        using var ffFrame = new FFFrame(format, chLayout, length.frame);
+        var frame = ffFrame.Frame;
+
+        using var ffEncoder = new FFEncoder(AVCodecID.AV_CODEC_ID_FIRST_AUDIO, format, &chLayout);
+        var encoder = ffEncoder.CodecCtx;
+
+        using var resampler = new FFmpegResampler((IntPtr)encoder,
+            format, encoder->ch_layout, encoder->sample_rate, false);
+
+        // act
+        var result = resampler.WriteFrame(frame, data, out var netRemainingSamples);
+
+        // assert
+        Assert.That(result, Is.True);
+        Assert.That(netRemainingSamples, Is.EqualTo(length.@out));
+        var cutData = SampleDataHelper.CutSamples(data, format, chLayout, length.@out);
+        CollectionAssert.AreEqual(cutData, ffFrame);
+    }
 }
